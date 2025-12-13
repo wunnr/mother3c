202dba4da90fd7729ff21c1b1be2d3df2fca6a11
StableCrimson <rhysethompson@gmail.com>
2025-12-12 20:38:57 -0600

    Match sub_08049F10

--- src/code_0803D59C.cpp
+++ src/code_0803D59C.cpp
@@ -420,7 +420,13 @@ extern "C" void sub_08049EE8() {
     }
 }
 
-extern "C" ASM_FUNC("asm/non_matching/code_0803D59C/sub_08049F10.inc", void sub_08049F10());
+extern "C" void sub_08049F10() {
+    if (gSomeBlend._4261 != 7) {
+        sub_0804A1C0(0x17);
+        gSomeBlend._4261 = 7;
+    }
+}
+
 extern "C" ASM_FUNC("asm/non_matching/code_0803D59C/sub_08049F38.inc", void sub_08049F38());
 extern "C" ASM_FUNC("asm/non_matching/code_0803D59C/sub_08049F60.inc", void sub_08049F60());
 extern "C" ASM_FUNC("asm/non_matching/code_0803D59C/sub_08049F88.inc", void sub_08049F88());
