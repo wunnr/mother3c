b6bf28dbba86c2e405b2f46b66e3f8883d8da254
StableCrimson <rhysethompson@gmail.com>
2025-12-17 07:36:04 -0600

    Extract gGBPlayerLogoGfx

--- data/gbPlayerLogo.s
+++ data/gbPlayerLogo.s
@@ -1,9 +1,5 @@
     .section .rodata
 
-    .global gGBPlayerLogoGfx
-gGBPlayerLogoGfx:
-    .incbin "baserom.gba", 0x000C7FE8, 0x4000
-
     .global gGBPlayerLogoLayout
 gGBPlayerLogoLayout:
     .incbin "baserom.gba", 0x000CBFE8, 0x500
