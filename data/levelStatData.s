17886c131927779412b96321255c8b38594b23f6
StableCrimson <rhysethompson@gmail.com>
2025-12-21 12:00:14 -0600

    Collapse gLevelStatTable

--- data/levelStatData.s
+++ data/levelStatData.s
@@ -2,8 +2,5 @@
 
     .global gLevelStatTable
 gLevelStatTable:
-    .incbin "baserom.gba", 0x000CC4E8, 0x4
-
-    .global gLevelStatTable2
-gLevelStatTable2:
-    .incbin "baserom.gba", 0x000CC4EC, 0x143C
+    .incbin "baserom.gba", 0x000CC4E8, 0x1440
+    
