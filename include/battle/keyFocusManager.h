f38800de7ffd92b4fe9cd006ec65db5afe095d60
wunnr <wunner@protonmail.com>
2026-02-07 17:26:50 -0700

    Move Sound class out of KeyFocusManager and into its own files

--- include/battle/keyFocusManager.h
+++ include/battle/keyFocusManager.h
@@ -12,37 +12,4 @@ public:
     virtual ~KeyFocuser();
 };
 
-struct Sound : Base {
-    Sound();
-    Sound(u16);
-    virtual ~Sound();
-
-    virtual bool setup(u16);
-    virtual void play(s32 fade);
-    virtual void stop(s32 fade);
-    virtual void continue_play(s32 fade);
-    virtual void clear(s32 fade);
-    virtual bool isPlaying();
-    virtual bool sound_1f8();
-    virtual bool sound_250();
-    virtual void setTempo(s32);
-    virtual void setVolume(s32);
-    virtual void setPan(s32);
-    virtual u16 getIndex();
-    virtual u32 getTempo();
-    virtual u32 getVolume();
-    virtual s8 getPan();
-    virtual u32 getTimestamp();
-    virtual u32 getPlayerClock();
-    
-    s32 fixFade(s32);
-
-    u16 idx;
-    const MusicPlayer* player;
-    u32 tempo;
-    u32 volume;
-    s8 pan;
-    u32 time;
-};
-
 #endif  // BATTLE_KEY_FOCUS_MANAGER_H
